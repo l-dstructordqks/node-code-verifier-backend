c4a437219591d57a7886610ea3d9a65720e9ba53
l-dstructordqks <s404990099@gmail.com>
2025-07-14 17:40:14 +0200

    webpack done & tsoa installed for swagger

--- src/controller/HelloController.ts
+++ src/controller/HelloController.ts
@@ -1,10 +1,18 @@
+import { Get, Query, Route, Tags } from "tsoa"; // Documentation usage
 import { BasicResponse } from "./types";
 import { IHelloController } from "./interfaces";
 import { LogSuccess } from "../utils/logger"; // Reference from "params" de tsconfig.json
 
+@Route("/api/hello")
+@Tags("HelloController")
 export class HelloController implements IHelloController {
-
-    public async getMessage(name?: string): Promise<BasicResponse> {
+    /**
+     * Endpoint to retrive a Message "Hello {name}" in JSON
+     * @param { string | undefined } name Name of user to be greeted
+     * @returns { BasicResponse } Promise of Basicresponse
+     */
+    @Get("/")
+    public async getMessage(@Query()name?: string): Promise<BasicResponse> {
         LogSuccess('[/api/hello] Get Request');
 
         return {
--- src/server/index.ts
+++ src/server/index.ts
@@ -19,6 +19,9 @@ server.use(
     rootRouter
     );
 
+// Static server
+server.use(express.static('public'));
+
 // TODO: Mongoose Connection
 
 // * Security config
